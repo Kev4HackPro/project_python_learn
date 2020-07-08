08c43c0c84891dfb6a123462b5c7a844d379ada1
Kavita <kavitakelvin07@gamil.com>
2020-07-08 04:45:56 +0300

    doing some revision

--- python_properties/bank_account(decorators).py
+++ python_properties/bank_account(decorators).py
@@ -1,83 +1,76 @@
 class Account:
-    """" A class used to represent a type of account """
 
     instance_count = 0
 
     @classmethod
     def increment_instance_count(cls):
-        print('Creating new Account')
+        print('New Account Created')
         cls.instance_count += 1
 
-    def __init__(self, account_number, account_holder, opening_balance, account_type):
+    def __init__(self, account_number, account_holder, opening_balance, type_of_account):
         Account.increment_instance_count()
-        self.account_number = account_number
-        self.account_holder = account_holder
-        self._open_bal = opening_balance
-        self.type = account_type
+        self.ac_no = account_number
+        self.ac_holder = account_holder
+        self._balance = opening_balance
+        self.type = type_of_account
 
     def deposit(self, amount):
-        self._open_bal += amount
+        self._balance += amount
 
     def withdraw(self, amount):
-        self._open_bal -= amount
+        self._balance -= amount
 
     @property
     def balance(self):
-        """Provides current balance"""
-        return self._open_bal
+        return self._balance
 
     def __str__(self):
-        return f'Account[{self.account_number}] - {self.account_holder}, {self.type} account ={self._open_bal}'
+        return f'[{self.ac_no}] {self.ac_holder} {self.type}={self._balance}'
 
 
 class CurrentAccount(Account):
 
     def __init__(self, account_number, account_holder, opening_balance, overdraft_limit):
-        super().__init__(account_number, account_holder, opening_balance, 'current')
-        self.overdraft_limit = -overdraft_limit
+        super().__init__(account_number, account_holder, opening_balance, 'current account')
+        self.ovdrft_limit = -overdraft_limit
 
     def withdraw(self, amount):
-        if self._open_bal - amount < self.overdraft_limit:
-            print('Withdrawal would exceed your overdraft limit')
+        if self.ovdrft_limit > self._balance - amount:
+            print('Withdrawal will exceed your overdraft limit')
         else:
-            self._open_bal -= amount
+            self._balance -= amount
 
     def __str__(self):
-        return f'{super().__str__()} overdraft limit:{self.overdraft_limit}'
+        return f'{super().__str__()}, overdraft_limit = {self.ovdrft_limit}'
 
 
 class DepositAccount(Account):
-
     def __init__(self, account_number, account_holder, opening_balance, interest_rate):
-        super().__init__(account_number, account_holder, opening_balance, 'deposit')
-        self.interest_rate = interest_rate
+        super().__init__(account_number, account_holder, opening_balance, 'deposit account')
+        self.interest = interest_rate
 
     def __str__(self):
-        return f'{super().__str__()}  interest rate: {self.interest_rate}'
+        return f'{super().__str__()} interest_rate={self.interest}'
 
 
 class InvestmentAccount(Account):
     def __init__(self, account_number, account_holder, opening_balance, investment_type):
-        super().__init__(account_number, account_holder, opening_balance, 'investment')
-        self.investment_type = investment_type
+        super().__init__(account_number, account_holder, opening_balance, 'investment account')
+        self.investment = investment_type
 
     def __str__(self):
-        return f"{super().__str__()} type:{self.type}"
+        return f"{super().__str__()} investment_type = {self.investment}"
 
 
-acc1 = CurrentAccount('123', 'Kavita', 23450, 100000)
+acc1 = CurrentAccount('789006', 'James Maina Kariuki', 97000, 600000)
 print(acc1)
-acc2 = DepositAccount('345', 'Kavita', 23550, 0.5)
+acc2 = DepositAccount('456789', 'Eunice Nyasuguta', 45978, 0.12)
 print(acc2)
-acc3 = InvestmentAccount('567', 'Sunaina', 12450, 'high risk')
+acc3 = InvestmentAccount('4467865', 'Marion Karanja', 560000, 'safe')
 print(acc3)
 
-acc1.deposit(23490)
-acc1.withdraw(12330)
-print('balance:', acc1.balance)
-
-print('Number of Account instances created:', Account.instance_count)
-
-print('balance:', acc1.balance)
-acc1.withdraw(300000.00)
-print('balance:', acc1.balance)
+acc1.deposit(450000)
+acc1.withdraw(78000)
+print('Balance', acc1.balance)
+acc1.withdraw(7900000)
+print('Balance', acc1.balance)
--- python_properties/setter_getter.py
+++ python_properties/setter_getter.py
@@ -3,32 +3,21 @@ class Person:
         self._name = name
         self._age = age
 
-    @property
-    def age(self):
-        """Age property"""
-        print('In age method')
+    def get_age(self):
         return self._age
 
-    @age.setter
-    def age(self, new_age):
+    def set_age(self, new_age):
         if isinstance(new_age, int) and 0 < new_age < 120:
             self._age = new_age
 
-    @property
-    def name(self):
-        print('In name method')
+    def get_name(self):
         return self._name
-
-    @name.deleter
-    def name(self):
-        del self._name
 
     def __str__(self):
         return f"Person[{self._name}] is {self._age}"
 
 
-p = Person('Kamau Karanja', 45)
+p = Person('John Waluke', 50)
 print(p)
-p.age = 78
+p.set_age(78)
 print(p)
-
