fc6925bc0f9c1e337f58a2f3c876f90856848225
Kavita <kavitakelvin07@gamil.com>
2020-07-10 03:37:36 +0300

    added polymorphism.py

---
+++ protocols_polymorphism_descriptors/protocols.py
@@ -0,0 +1,22 @@
+class ContextManagedClass(object):
+    def __init__(self):
+        print("__init__")
+
+    def __enter__(self):
+        print('__enter__')
+        return self
+
+    def __exit__(self, *args):
+        print('__exit__:', args)
+        return True
+
+    def __str__(self):
+        return 'ContextManagedClass object'
+
+
+print('Starting')
+with ContextManagedClass() as cmc:
+    print('In with block', cmc)
+    print('Exiting')
+
+print('Done')
