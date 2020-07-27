0be3971a7c98bd7456756f4e4ff62dba9ebfbf65
Kavita <kavitakelvin07@gmail.com>
2020-07-27 09:18:29 +0300

    found an easier way of doing factorial by importing factorial function from math module

--- iteration/exercise1.py
+++ iteration/exercise1.py
@@ -1,12 +1,15 @@
-number = input('Number you want factorial for: ')
-if number.isnumeric():
-    new_number = int(number)
-    if new_number == 0:
-        print('factorial for zero is 1')
-    else:
-        factorial = 1
-        for item in range(1, new_number + 1):
-            factorial = factorial * item
-        print(f"{number}! is {factorial}")
-else:
-    print('Number entered is not positive')
+from math import factorial
+number = int(input('Number you want factorial for: '))
+
+print(factorial(number))
+# if number.isnumeric():
+#     new_number = int(number)
+#     if new_number == 0:
+#         print('factorial for zero is 1')
+#     else:
+#         factorial = 1
+#         for item in range(1, new_number + 1):
+#             factorial = factorial * item
+#         print(f"{number}! is {factorial}")
+# else:
+#     print('Number entered is not positive')
