b545a7f23e5e0556cc650ae8a053fdacf1e796be
Kavita <kavitakelvin07@gmail.com>
2020-08-01 05:18:01 +0300

    added authors code

--- collection_related_modules/concordance_exercise.py
+++ collection_related_modules/concordance_exercise.py
@@ -8,3 +8,26 @@ print('Ordered word count')
 alphabetic_list = OrderedDict(list_sentence)
 print(alphabetic_list)
 
+# Author's code
+word_count = Counter()
+input_string = input('Enter your sentence here: ')
+
+split_sentence = input_string.split(' ')
+for words in split_sentence:
+    if words == '':
+        pass
+    elif words in word_count:
+        word_count[words] += 1
+    else:
+        word_count[words] = 1
+
+print('Unordered Counter')
+print(word_count)
+
+ordered_word_count = OrderedDict()
+for key in sorted(word_count):
+    ordered_word_count[key] = word_count[key]
+
+print('Ordered Word Count')
+print(ordered_word_count)
+
